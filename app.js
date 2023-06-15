edeb5c68158fba0d62d1e2f6d0eba7826c932234
[Marina Suarez] <[suarezmarina.e@gmail.com]>
2023-06-15 13:57:47 -0300

    actividades de practica en js

---
+++ app.js
@@ -0,0 +1,111 @@
+//Crear programas.
+// Ejercicio 1
+// let numero = 6;
+// if (numero % 2 == 0){
+//     console.log("El numero es par")}
+// else {
+//     console.log("El numero es impar")
+// };
+
+// Ejercicio 2
+// let numero = 8;
+// if (numero > 0){
+//     console.log("El numero es positivo")}
+// else {
+//     console.log("El numero es negativo")
+// };
+
+// Ejercicio 3
+// let cadenaTexto = "Hola soy cadena";
+// if (cadenaTexto.length > 10){
+//     console.log("Largo")}
+// else {
+//     console.log("Corto")
+// };
+
+// Arrays
+// Ejercicio 1
+// let arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i=0; i<arrayNumeros.length; i++){
+//     console.log(i)
+// };
+
+// Ejercicio 2 (Solicité ayuda)
+// var amigos = ["Mailen", "Tomas", "Paz"];
+// var lista = document.getElementById("amigos-lista");
+// amigos.forEach(function(amigo){
+//     var elementoLista = document.createElement("li");
+//     elementoLista.textContent = amigo;
+//     lista.appendChild(elementoLista);
+// });
+
+//Ejercicio 3
+// let edades = [73, 72, 33, 30, 28, 32];
+// let suma = 0;
+// for (var i = 0; i<edades.length; i++){
+//     suma = suma + edades [i];
+// }
+// let promedio = (suma/edades.length);
+// console.log(promedio)
+
+//Bucle for con arrays
+//Ejercicio 1
+// let colores = ["verde", "rojo", "negro", "blanco", "amarillo"];
+// for (var i=0; i<colores.length; i++){
+//     console.log(colores)
+// };
+
+//Ejercicio 2
+// var frutas = ["banana", "manzana", "melon", "pera", "ciruela"];
+// var lista = document.getElementById("frutas-lista");
+// frutas.forEach(function(fruta){
+//    var elementoLista = document.createElement("li");
+//    elementoLista.textContent = fruta;
+//    lista.appendChild(elementoLista);
+// });
+
+//Ejercicio 3 Consultar
+//let numeros = [1, 4, 6, 7, 20, 18];
+
+
+//Funciones
+//Ejercicio 1
+// function sumarNumerosPares(n){
+//     var suma = 0;
+//     for (var i=1; i<=n; i++){
+//         if (i%2 == 0){
+//             suma += i
+//         }
+//     }
+//     return suma
+// }
+// console.log(sumarNumerosPares(6))
+
+//Ejercicio 2
+// function calcularImpuestos(x){
+//     var impuesto = 0;
+//     for (var i=0; i<x; i++){
+//         if (x <1000){
+//             impuesto += (10/100)
+//         }
+//         if (x >=1000){
+//             impuesto += (20/100)
+//         }
+//     }
+//     return impuesto
+// }
+// console.log(calcularImpuestos(1500))
+
+//Ejercicio 3
+function contarVocales(x){
+    var vocales = 0;
+    for (var i=0; i<x.length; i++){
+        if (x)
+    }
+    return vocales
+}
+console.log(contarVocales(x))
+
+//Ejercicio 4
+//function generarContraseña(x)
+
